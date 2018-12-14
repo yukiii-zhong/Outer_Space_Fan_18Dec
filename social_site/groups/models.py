2f49481f56021b3517f56b75465dd49f476b17a0
yukizhong <yukiiizhong@gmail.com>
2018-12-13 21:19:53 -0500

    Add slug, save function, get_absolute_url to group.models

--- social_site/groups/models.py
+++ social_site/groups/models.py
@@ -12,13 +12,22 @@ register = template.Library()
 
 class Group(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    # slug = models.SlugField(allow_unicode=True, unique=True)
-    description = models.TextField(blank= True, default = '')
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
     description_html = models.TextField(editable=False, default='', blank=True)
-    # members = models.ManyToManyField(User, through="GroupMember")
+    members = models.ManyToManyField(User,through="GroupMember")
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+
 
     class Meta:
         ordering = ["name"]
