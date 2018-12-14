d8f84a70a502fcd9150996a0be3659be21db40a0
yukizhong <yukiiizhong@gmail.com>
2018-12-13 20:24:32 -0500

    Accounts app

--- social_site/accounts/views.py
+++ social_site/accounts/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import login, logout
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+
+from . import forms
 
 # Create your views here.
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
--- social_site/social_site/settings.py
+++ social_site/social_site/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'accounts',
+    'groups',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'social_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
